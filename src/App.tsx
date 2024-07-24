e81a93cdbe928fb3a85370efd97d7f3a481a9dba
SohamNagi <nagisoham@gmail.com>
2024-07-24 17:11:56 -0400

    created KPI Placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "./components/footer";
 import Header from "./components/header";
-import Graphs from "./components/graphs";
+import Impressions from "./components/impressions";
+import Clicks from "./components/clicks";
+import KPI from "./components/kpi";
+import cdata from "./mock_campaign_results.json";
+
 
 function App() {
+  console.log(cdata)
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex flex-col items-center justify-between h-screen">
         <Header></Header>
-        <Graphs></Graphs>
+        <div>
+          <Impressions></Impressions>
+          <KPI></KPI>
+          <Clicks></Clicks>
+        </div>
         <Footer></Footer>
       </div>
     </ThemeProvider>
---
+++ src/components/kpi.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const KPI = () => {
+  return (
+    <div>
+      <div className="flex pb-5  flex-row">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-extrabold">
+              Most Clicks
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl">
+              c
+            </div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-extrabold">Best Day</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl">
+              c
+            </div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-extrabold">
+              Most Impressions
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl ">
+              c
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default KPI;
